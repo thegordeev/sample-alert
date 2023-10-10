7e98f45a66217a8e3466d18e447a539fea947a9d
egorgordeev <5tA1atH0mPLS>
2023-10-10 16:05:26 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDmkGDpbZUratnbhGJhPCaiIRqP6B3AteY",
+  authDomain: "sample-alert-c70ac.firebaseapp.com",
+  projectId: "sample-alert-c70ac",
+  storageBucket: "sample-alert-c70ac.appspot.com",
+  messagingSenderId: "448468394276",
+  appId: "1:448468394276:web:551b297deffd4648c6d3b1",
+  measurementId: "G-S63SCP51M2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 function App() {
   return (
